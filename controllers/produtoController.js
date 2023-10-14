6c605a966509d826b724661e9e0611945c1d10fb
Giovani <systemtech.g.g@gmail.com>
2023-10-14 16:32:08 -0300

    inproving mvc structure

---
+++ controllers/produtoController.js
@@ -0,0 +1,71 @@
+const Produto = require('../models/produto');
+
+exports.criarProduto = async (req, res) =>{
+
+    try { 
+      const novoProduto = await Produto.create(req.body);
+      res.status(201).json(novoProduto);
+    } catch (error) {
+      res.status(400).json({ error: 'Não foi possível criar o produto.' });
+    }
+};
+
+exports.listarProdutos = async (req, res) => {
+
+    try {
+        const produtos = await Produto.findAll();
+        res.json(produtos);
+      } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar produtos.' });
+      }
+};
+
+exports.buscarProdutoId = async (req, res)=>{
+
+    const parametroid = req.params.id;
+    const produto = await Produto.findByPk(parametroid)
+    try{
+        if (produto) {
+            res.json(produto);
+          } else {
+            res.status(404).json({ error: 'Produto não encontrado.' });
+          }
+    } catch (error){
+        res.status(500).json({ error: 'Erro ao obter informações do produto.' });
+    }
+};
+
+exports.atualizarProduto =  async (req, res)=>{
+
+    const parametroid = req.params.id;
+    const [atualizado] = await Produto.update(req.body, { where: {id: parametroid } });
+    
+    try{
+        if (atualizado) {
+            res.json({ message: 'Produto atualizado com sucesso.' });
+          } else {
+            res.status(404).json({ error: 'Produto não encontrado.' });
+          }
+    } catch (error){
+        res.status(500).json({ error: 'Erro ao atualizar informações do produto.' });
+    }
+    
+};
+
+exports.deletarProduto = async (req, res)=>{
+
+    const parametroid = req.params.id;
+    const deletado = await Produto.destroy({ where: {id: parametroid } });
+
+  try{
+        if (deletado) {
+            res.json({ message: 'Produto excluído com sucesso.' });
+        } else {
+            res.status(404).json({ error: 'Produto não encontrado.' });
+        }
+  } catch (error){
+    res.status(500).json({ error: 'Erro ao excluir o produto.' });
+  }
+  
+
+};
--- routes.js
+++ routes.js
@@ -1,73 +1,27 @@
 
-const Produto = require('./models/produto');
+
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+const produtoController = require('./controllers/produtoController');
 
- //Produto.sync({force: true}); //codigo para criar a tabela no banco de dados, deve ser utilizado só uma vez por tabela
+//Produto.sync({force: true}); //codigo para criar a tabela no banco de dados, deve ser utilizado só uma vez por tabela
 
 //criar um novo produto
-app.post('/addproduto', async (req, res) => {
-    try {
-        console.log('Corpo da solicitação:', req.body);
-      const novoProduto = await Produto.create(req.body);
-      res.status(201).json(novoProduto);
-    } catch (error) {
-      res.status(400).json({ error: 'Não foi possível criar o produto.' });
-    }
-  });
+app.post('/addproduto', produtoController.criarProduto);
 
 //listar todos os produtos
-app.get('/produtos', async (req, res) => {
-
-    try {
-      const produtos = await Produto.findAll();
-      res.json(produtos);
-    } catch (error) {
-      res.status(500).json({ error: 'Erro ao buscar produtos.' });
-    }
-  });
+app.get('/produtos', produtoController.listarProdutos);
 
 //listar um produto pelo id 
- app.get('/produtos/:id', async (req, res) => {
-
-    const parametroid = req.params.id;
-    const produto = await Produto.findByPk(parametroid)
-    
-    if (produto) {
-      res.json(produto);
-    } else {
-      res.status(404).json({ error: 'Produto não encontrado.' });
-    }
-
-  });
+ app.get('/produtos/:id', produtoController.buscarProdutoId);
 
 //buscar um produto pelo id para atualizar suas informações 
-  app.put('/updateprodutos/:id', async (req, res) => {
-
-    const parametroid = req.params.id;
-    const [atualizado] = await Produto.update(req.body, { where: {id: parametroid } });
-  
-    if (atualizado) {
-      res.json({ message: 'Produto atualizado com sucesso.' });
-    } else {
-      res.status(404).json({ error: 'Produto não encontrado.' });
-    }
-  }); 
+  app.put('/updateprodutos/:id', produtoController.atualizarProduto);
 
 //buscar um produto pelo id e excluir o mesmo
-app.delete('/deleteprodutos/:id', async (req, res) => {
-
-      const parametroid = req.params.id;
-      const deletado = await Produto.destroy({ where: {id: parametroid } });
-     
-    if (deletado) {
-      res.json({ message: 'Produto excluído com sucesso.' });
-    } else {
-      res.status(404).json({ error: 'Produto não encontrado.' });
-    }
-
-  });
+app.delete('/deleteprodutos/:id', produtoController.deletarProduto);
   
   module.exports = app;
